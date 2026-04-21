c6db09bfd0ac8e56297ed7ec1dc6ef5a5e3c3aca
baiel <raimkulovbaiel2@gmail.com>
2026-04-21 13:12:48 +0600

    added ClinicCard

--- src/entities/clinic/ui/ClinicCard.tsx
+++ src/entities/clinic/ui/ClinicCard.tsx
@@ -14,9 +14,9 @@ export default function ClinicCard({
   bgColor = "#FFFFFF",
 }: ClinicCardProps) {
   return (
-    <article className="w-full max-w-[302px] sm:max-w-[302px] lg:max-w-[302px]">
+    <article className="w-full max-w-75.5 sm:max-w-75.5 lg:max-w-75.5">
       <div
-        className="flex h-[170px] w-full items-center justify-center rounded-[14px] border-2 sm:h-[220px] lg:h-[302px] lg:w-[302px] "
+        className="flex h-42.5 w-full items-center justify-center rounded-[14px] border-2 sm:h-55 lg:h-75.5 lg:w-75.5 "
         style={{
           borderColor,
           backgroundColor: bgColor,
