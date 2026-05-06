9b86edcc861f4cc4f252cfaab56e694e9c673df4
baiel <raimkulovbaiel2@gmail.com>
2026-05-06 22:37:27 +0600

    test ReviewsCopy

--- src/app/ClinicProfile/page.tsx
+++ src/app/ClinicProfile/page.tsx
@@ -2,11 +2,13 @@ import ClinicProfile from "@/widgets/ClinicProfile/ui/ClinicProfile";
 import ClinicServices from "@/widgets/ClinicServices/ui/ClinicServices"; 
 import Specialists from "@/widgets/Specialists/ui/Specialists"; 
 import Specialistssearch from "@/features/Specialists-search/ui/Specialists";
+import ReviewsCopy from "@/widgets/ReviewsCopy/ui/ReviewsCopy";
 
 export default function Home() {
   return <><ClinicProfile />
     <ClinicServices /> 
     <Specialistssearch/>  
-    <Specialists/> 
+    <Specialists/>  
+    <ReviewsCopy/>
     </>;
 }
