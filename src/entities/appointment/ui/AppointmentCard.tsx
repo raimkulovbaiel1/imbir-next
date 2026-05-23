102c7fedba218459facf805435fd7fb8aa26fefd
baiel <raimkulovbaiel2@gmail.com>
2026-05-23 19:41:39 +0600

    test  AppointmentCard

--- src/entities/appointment/ui/AppointmentCard.tsx
+++ src/entities/appointment/ui/AppointmentCard.tsx
@@ -13,7 +13,6 @@ export default function AppointmentCard({
 }: Props) {
   return ( 
     <div className="  bg-white rounded-[16px]  p-4 md:p-5 flex flex-col md:flex-row  gap-4 md:justify-between md:items-start ">
-      {/* LEFT */}
       <div>
         <p className="text-[12px] md:text-[16px] text-gray-500">
           {date}
@@ -32,7 +31,6 @@ export default function AppointmentCard({
         </p>
       </div>
 
-      {/* RIGHT */}
       <div className=" flex flex-row md:flex-col items-center md:items-end justify-between gap-3 w-full md:w-auto ">
         <button className=" bg-[#FF0000]  text-white text-[12px] md:text-[16px] px-3 py-1 rounded whitespace-nowrap cursor-pointer">
           ✖ Отменить
