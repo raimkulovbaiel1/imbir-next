130d13ef7b568eb1c99bd53b3c3a506bd0506ca4
baiel <raimkulovbaiel2@gmail.com>
2026-08-29 21:00:50 +0600

    refab khl

--- src/pages/procedures/ui/ProceduresPage.tsx
+++ src/pages/procedures/ui/ProceduresPage.tsx
@@ -1,20 +1,32 @@
 "use client";
 
-//import ProceduresSearch from "@/features/procedures-search/ui/ProceduresSearch"; 
+import { useState } from "react";
+
 import ProceduresSearch from "@/features/procedures-search/ui/ProceduresSearch";
-//import ProceduresFilters from "@/features/procedures-filters/ui/ProceduresFilters"; 
 import ProceduresFilters from "@/features/procedures-filters/ui/ProceduresFilters";
-//import ProceduresList from "@/widgets/procedures-list/ui/ProceduresList";  
 import ProceduresList from "@/widgets/procedures-list/ui/ProceduresList";
 
-
 export default function ProceduresPage() {
+  const [search, setSearch] = useState("");
+
   return (
     <section className="min-h-screen bg-[#efefef] py-8">
       <div className="mx-auto w-full max-w-[1220px] pb-[40px] px-4">
-        <ProceduresSearch />
+
+        {/* ПОИСК */}
+        <ProceduresSearch
+          value={search}
+          onChange={setSearch}
+        />
+
+        {/* ФИЛЬТРЫ */}
         <ProceduresFilters />
-        <ProceduresList />
+
+        {/* ПРОЦЕДУРЫ */}
+        <ProceduresList
+          search={search}
+        />
+
       </div>
     </section>
   );
